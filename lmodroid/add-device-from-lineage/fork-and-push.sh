93d18c260d42f3a537f2b8854d8c67a02cd3258a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-07-29 10:08:11 +0530

    Workflow to adapt and push device (#1)
    * adapt: Create github actions workflows
    Also disable gerrit stuff
    
    * adapt: Fix git clone line
    
    * Workflow: Set git username and email
    
    * Hack: Try to get token from file
    
    * Hack: Rename GITHUB_TOKEN to PAT
    For some reason, git isn't playing nicely and is repeatedly asking for username. Hope this fixes it
    
    * Give up and make fork and push script work in adapt.yml
    
    * Undo PAT shenanigans
    
    * Fix path to python script
    
    * adapt.yml: fix path to script
    
    * adapt.yml: Drop variable
    
    * adapt.yml: try to use git clone
    
    * adapt.yml: Fix the input handling logic
    
    * adapt.yml: Fix variables
    
    * adapt.yml: Use PAT to clone script
    
    * adapt.yml: Support external repos
    
    * adapt.yml: Change token variable name
    
    * adapt.yml: Move token to env variables
    
    * adapt.yml: Make git clone conditional
    
    * adapt.yml: Check usernames instead
    
    * adapt.yml: Fix syntax
    
    * Update adapt.yml
    
    * adapt.yml: Fix syntax
    
    * adapt.yml: Fix syntax
    
    * adapt.yml: Fix logic

--- lmodroid/add-device-from-lineage/fork-and-push.sh
+++ lmodroid/add-device-from-lineage/fork-and-push.sh
@@ -11,6 +11,16 @@ if [ "$4" == "" ]; then
     exit 1
 fi
 
+if [[ ! -f "token" ]]; then
+if [[ -z "$PAT" ]]; then
+    echo "ERROR: Set the GITHUB_TOKEN environment variable"
+    exit 1
+fi
+echo "$GITHUB_TOKEN" > token
+fi
+# Authenticate using GH
+gh auth login --with-token < token
+
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
@@ -19,7 +29,7 @@ branch_src=$2
 repo_dist=$3
 branch_dist=$4
 
-ssh -p 29418 -o StrictHostKeyChecking=no ${GERRIT_USER}@gerrit.libremobileos.com gerrit create-project --parent LMODroid-Device-Projects ${repo_dist}
+#ssh -p 29418 -o StrictHostKeyChecking=no ${GERRIT_USER}@gerrit.libremobileos.com gerrit create-project --parent LMODroid-Device-Projects ${repo_dist}
 
 if [[ ! -d working ]]; then
     mkdir -p working
@@ -31,8 +41,8 @@ if [[ -d ${repo_dist_dir} ]]; then
     rm -rf ${repo_dist_dir}
 fi
 
-git clone ssh://${GERRIT_USER}@gerrit.libremobileos.com:29418/${repo_dist} 
-scp -p -P 29418 ${GERRIT_USER}@gerrit.libremobileos.com:hooks/commit-msg "${repo_dist_dir}/.git/hooks/"
+git clone https://github.com/${repo_dist}.git 
+#scp -p -P 29418 ${GERRIT_USER}@gerrit.libremobileos.com:hooks/commit-msg "${repo_dist_dir}/.git/hooks/"
 if [[ ! -d ${repo_dist_dir} ]]; then
     echo "ERROR: Repo failed to clone"
     exit 1
